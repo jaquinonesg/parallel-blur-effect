9f9441e5681b227c9a92271949d1273eca27f0e3
Laura Álvarez <lvalvarezc@unal.edu.co>
2017-10-25 00:09:49 -0500

    Generate blur using threads

--- code/ejecutar.sh
+++ code/ejecutar.sh
@@ -1,1 +1,2 @@
-g++ `pkg-config --cflags opencv` test.cpp `pkg-config --libs opencv` -o test.out
+# g++ `pkg-config --cflags opencv` test.cpp `pkg-config --libs opencv` -o test.out
+g++ `pkg-config --cflags opencv` test-par.cpp `pkg-config --libs opencv` -o test.out -lpthread
